056e44a5128aa6f35d9916e69e6d178ba9404fbc
kunal2604 <kmrknl2604@gmail.com>
2018-09-29 08:02:33 +0530

    static_files

--- myproject/settings.py
+++ myproject/settings.py
@@ -93,12 +93,14 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-'''STATICFILES_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-]'''
+]
+'''
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
+'''
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
